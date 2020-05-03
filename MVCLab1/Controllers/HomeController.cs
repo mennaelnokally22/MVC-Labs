f10cfa8ee12d4d8c90a5162339299c395fb3e180
Menna <menna.elnokally@gmail.com>
2020-05-03 16:01:52 +0200

    first commit

---
+++ MVCLab1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public class User
+        {
+            public string name { get; set; }
+            public string email { get; set; }
+            public string phone { get; set; }
+        }
+        public ViewResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvForm(User user)
+        {
+            if(user != null && user.name != null && user.email != null)
+            {
+                ViewBag.Name = user.name;
+                return View("Welcome");
+            }
+            return View();
+        }
+
+    }
+}

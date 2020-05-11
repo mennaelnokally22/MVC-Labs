0f3572faf515974f6f11fcd9e78501f152b20910
Menna <menna.elnokally@gmail.com>
2020-05-11 17:29:58 +0200

    Lab 4

--- MVCLab2/Controllers/EmployeesController.cs
+++ MVCLab2/Controllers/EmployeesController.cs
@@ -1,6 +1,7 @@
 ﻿using MVCLab2.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -31,6 +32,32 @@ namespace MVCLab2.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                mc.Employees.Attach(employee);
+                mc.Entry(employee).State = EntityState.Modified;
+                mc.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            Employee employee = mc.Employees.Find(id);
+            if (employee != null)
+            {
+                mc.Employees.Remove(employee);
+                mc.SaveChanges();
+                return Json(true);
+            }
+            return HttpNotFound("Employee not found!");
         }
     }
 }
